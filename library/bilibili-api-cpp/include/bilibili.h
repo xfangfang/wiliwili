c006f6fe41e769ebd6e280bcf264647c7703af79
xfangfang <2553041586@qq.com>
2021-01-21 07:35:13 +0800

    Add Net Image

--- library/bilibili-api-cpp/include/bilibili.h
+++ library/bilibili-api-cpp/include/bilibili.h
@@ -17,6 +17,9 @@ namespace bilibili {
             BilibiliClient();
             void test(std::function<void(std::string)> callback);
             void get_top10(int rid, std::function<void(VideoList)> callback);
+            void get_top100(int rid, std::function<void(VideoList)> callback);
+            void get_playurl(std::string avid, std::string cid, std::function<void(std::string)> callback);
+            void download(std::string url, std::function<void(unsigned char *, size_t)> callback);
 
         private:
             Request request_common;
--- library/bilibili-api-cpp/source/bilibili.cpp
+++ library/bilibili-api-cpp/source/bilibili.cpp
@@ -28,6 +28,53 @@ namespace bilibili {
         );
     }
 
+    void BilibiliClient::get_top100(int rid, std::function<void(VideoList)> callback){
+        this->request_common = cpr::GetCallback([callback](cpr::Response r) {
+                nlohmann::json res = nlohmann::json::parse(r.text);
+                VideoList videoList = res.get<VideoList>();
+                callback(videoList);
+            },
+            cpr::Url{"http://api.bilibili.com/x/web-interface/ranking/region"},
+            cpr::Parameters{
+                {"rid","160"},
+                {"day","7"}
+            }
+        );
+    }
+
+    void BilibiliClient::get_playurl(std::string avid, std::string cid, std::function<void(std::string)> callback){
+        this->request_common = cpr::GetCallback([callback](cpr::Response r) {
+                callback(r.text);
+                // nlohmann::json res = nlohmann::json::parse(r.text);
+                // VideoList videoList = res.get<VideoList>();
+                // callback(videoList);
+            },
+            cpr::Url{"http://api.bilibili.com/x/player/playerurl"},
+            cpr::Parameters{
+                {"avid", "843694839"},
+                {"cid",  "283541679"},
+                {"qu",   "80"},
+                {"otype","json"},
+                {"fnval", "16"}
+            },
+            cpr::Header{
+                {"Referer","http://www.bilibili.com"}
+            }
+        );
+    }
+
+    void BilibiliClient::download(std::string url, std::function<void(unsigned char *, size_t)> callback){
+        this->request_common = cpr::GetCallback([callback](cpr::Response r) {
+                callback((unsigned char *)r.text.c_str(), (size_t)r.downloaded_bytes);
+            },
+            cpr::Url{url},
+            cpr::Header{
+                {"Referer","http://www.bilibili.com"},
+                {"Agent","NintendoSwitch"}
+            }
+        );
+    }
+
     void BilibiliClient::_common_get(std::string url, std::function<void(std::string)> callback){
         this->request_common = cpr::GetCallback([callback](cpr::Response r) {
                 callback(r.text);
