a2cf33f0d4d06a83e29b7eb3a77eb9b9b3cebb4b
xfangfang <2553041586@qq.com>
2022-08-31 02:14:21 +0800

    Fix: Adapt to API changes of bilbili

--- wiliwili/source/presenter/mine_collection.cpp
+++ wiliwili/source/presenter/mine_collection.cpp
@@ -5,6 +5,7 @@
 #include "bilibili.h"
 #include "presenter/mine_collection.hpp"
 #include "utils/config_helper.hpp"
+#include "borealis/core/logger.hpp"
 
 
 MineCollectionRequest::MineCollectionRequest(){
@@ -15,16 +16,27 @@ void MineCollectionRequest::onCollectionList(const bilibili::CollectionListResul
 void MineCollectionRequest::onError(const std::string& error) {}
 
 void MineCollectionRequest::requestData(bool refresh) {
-    static int index = 1;
-    if(refresh)
+    if(refresh){
         index = 1;
-    auto cookie = ProgramConfig::instance().getCookie();
-    this->requestCollectionList(cookie["DedeUserID"], index++);
+        hasMore = true;
+    }
+
+    if(hasMore){
+        auto userID = ProgramConfig::instance().getUserID();
+        this->requestCollectionList(userID, index);
+    }
 }
 
-void MineCollectionRequest::requestCollectionList(std::string& mid, int index, int num) {
-    bilibili::BilibiliClient::get_my_collection_list(mid, index, num,
+void MineCollectionRequest::requestCollectionList(std::string& mid, int i, int num) {
+    bilibili::BilibiliClient::get_my_collection_list(mid, i, num,
             [this](const bilibili::CollectionListResultWrapper &result) {
+                if(index != result.index){
+                    brls::Logger::error("MineCollectionRequest::requestCollectionList: request: {}, got: {}",
+                                        index, result.index);
+                    return;
+                }
+                hasMore = result.has_more;
+                index = result.index + 1;
                 this->onCollectionList(result);
             }, [this](const std::string &error) {
                 this->onError(error);
