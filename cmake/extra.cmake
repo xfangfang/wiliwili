0595e46cd3743332227f46238046d64796867705
xfangfang <2553041586@qq.com>
2023-07-15 08:35:21 +0800

    Cmake rewrite (#165)
    * cmake rewrite
    * ignore opencc when using libromfs

--- cmake/extra.cmake
+++ cmake/extra.cmake
@@ -1,12 +1,5 @@
 cmake_minimum_required(VERSION 3.15)
 
-# Add debug info
-IF (NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Debug" CACHE STRING
-            "Choose the type of build, options are: Debug Release RelWithDebInfo MinSizeRel." FORCE)
-    message(STATUS "Build Type: ${CMAKE_BUILD_TYPE}")
-ENDIF ()
-
 if (CMAKE_BUILD_TYPE STREQUAL Debug)
     add_definitions(-D_DEBUG)
     add_definitions(-D_GLIBCXX_ASSERTIONS)
@@ -16,23 +9,6 @@ if (CMAKE_BUILD_TYPE STREQUAL Debug)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined,address")
     endif ()
 endif ()
-
-# Add git info
-execute_process(COMMAND git describe --tags
-        TIMEOUT 5
-        OUTPUT_VARIABLE GIT_TAG_VERSION
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-execute_process(COMMAND git rev-parse --short HEAD
-        TIMEOUT 5
-        OUTPUT_VARIABLE GIT_TAG_SHORT
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-
-add_definitions(-DBUILD_TAG_VERSION=${GIT_TAG_VERSION} -DBUILD_TAG_SHORT=${GIT_TAG_SHORT})
-
-message(STATUS "building from git tag ${GIT_TAG_VERSION}")
-message(STATUS "building from git commit ${GIT_TAG_SHORT}")
 
 if (APPLE AND NOT PLATFORM_IOS)
     if (MAC_IntelChip)
