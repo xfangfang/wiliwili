b02bbdd885bcf3b7261d75383239561cd315b056
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-22 18:02:31 +0800

    try reduce overhead

--- wiliwili/source/api/util/extract_messages.cpp
+++ wiliwili/source/api/util/extract_messages.cpp
@@ -5,27 +5,6 @@
 
 #include <utility>
 
-// std::vector<std::string> extract_danmu_messages(const std::vector<std::string>& messages) {
-//     std::vector<std::string> danmu_messages;
-//     danmu_messages.reserve(messages.size()); // 预留空间
-
-//     for (const auto& message : messages) {
-//         //try {
-//             auto json_message = nlohmann::json::parse(message);
-
-//             if (json_message.contains("cmd") && json_message["cmd"] == "DANMU_MSG") {
-//                 if (json_message.contains("info") && json_message["info"].is_array() && json_message["info"].size() > 1) {
-//                     danmu_messages.emplace_back(json_message["info"][1].get<std::string>());
-//                 }
-//             }
-//         //} catch (const nlohmann::json::parse_error& e) {
-//         //   /std::cerr << "Failed to parse JSON message: " << e.what() << std::endl;
-//         //}
-//     }
-
-//     return danmu_messages;
-// }
-
 std::vector<std::string> extract_danmu_messages(const std::vector<std::string>& messages) {
 
     std::vector<std::string> danmu_messages;
