0db6c85796b340ddd2f582aba30330c8748e1002
蜻蜓特派员 <dragonflylee@outlook.com>
2023-07-26 14:22:36 +0800

    ci: autobuild nsp forwarder (#175)
    * ci: autobuild nsp forwarder

--- scripts/build_switch.sh
+++ scripts/build_switch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 BUILD_DIR=cmake-build-switch
@@ -6,26 +7,22 @@ BUILD_DIR=cmake-build-switch
 cd "$(dirname $0)/.."
 git config --global --add safe.directory `pwd`
 
-BASE_URL="https://github.com/xfangfang/wiliwili/releases/download/v0.1.0"
-LIBASS="switch-libass-0.17.1-1-any.pkg.tar.zst"
-FFMPEG="switch-ffmpeg-4.4.4-1-any.pkg.tar.zst"
-MPV="switch-libmpv-0.35.1-1-any.pkg.tar.zst"
-NSPMINI="switch-nspmini-48d4fc2-1-any.pkg.tar.xz"
+BASE_URL="https://github.com/xfangfang/wiliwili/releases/download/v0.1.0/"
 
-if [ ! -f "${LIBASS}" ];then
-    wget ${BASE_URL}/${LIBASS}
-fi
-if [ ! -f "${FFMPEG}" ];then
-    wget ${BASE_URL}/${FFMPEG}
-fi
-if [ ! -f "${MPV}" ];then
-    wget ${BASE_URL}/${MPV}
-fi
-if [ ! -f "${NSPMINI}" ];then
-    wget ${BASE_URL}/${NSPMINI}
-fi
+PKGS=(
+    "switch-libass-0.17.1-1-any.pkg.tar.zst"
+    "switch-ffmpeg-4.4.4-1-any.pkg.tar.zst"
+    "switch-libmpv-0.35.1-1-any.pkg.tar.zst"
+    "switch-nspmini-48d4fc2-1-any.pkg.tar.xz"
+)
+for PKG in "${PKGS[@]}"; do
+    [ -f "${PKG}" ] || curl -LO ${BASE_URL}${PKG}
+    dkp-pacman -U --noconfirm ${PKG}
+done
 
-dkp-pacman -U --noconfirm ${LIBASS} ${FFMPEG} ${MPV} ${NSPMINI}
+HACBREWPACK="hacbrewpack-v3.06_linux-amd64.tar.gz"
+[ -f "${HACBREWPACK}" ] || curl -LO https://github.com/dragonflylee/hacBrewPack/releases/download/v3.06/${HACBREWPACK}
+tar zxf ${HACBREWPACK} -C /usr/local/bin
 
 cmake -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release -DBUILTIN_NSP=ON -DPLATFORM_SWITCH=ON
 make -C ${BUILD_DIR} wiliwili.nro -j$(nproc)
