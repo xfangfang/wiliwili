b6d628a96eb610c9d1ad5f0202b4ad0a26d5d1d3
happytommyl <happytommyl@gmail.com>
2023-09-23 02:23:31 +0800

    add an option to share on different target

--- wiliwili/include/utils/clipboard_helper.hpp
+++ wiliwili/include/utils/clipboard_helper.hpp
@@ -6,26 +6,85 @@
 #include <winuser.h>
 #endif
 
+
+
+
 namespace wiliwili{
 
-    void toClipboard(const std::string& link) {
-        #ifdef _WIN32
-        auto hwnd = GetDesktopWindow();
-        auto len = link.c_str();
-        auto size = link.size();
-        OpenClipboard(hwnd);
-        EmptyClipboard();
-        HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, size+1);
-        if (!hg) {
-            CloseClipboard();
-            return;
+    enum shareTarget{
+        clipboard,
+        dynamic,
+        qq,
+        qzone,
+        tieba,
+        wechat,
+        weibo,
+    };
+
+    void toClipboard(const std::string& link, const shareTarget target) {
+        switch(target){
+            
+            case clipboard:
+            {
+                #ifdef _WIN32
+                auto hwnd = GetDesktopWindow();
+                auto len = link.c_str();
+                auto size = link.size();
+                OpenClipboard(hwnd);
+                EmptyClipboard();
+                HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, size+1);
+                if (!hg) {
+                    CloseClipboard();
+                    return;
+                }
+                memcpy(GlobalLock(hg), len, size+1);
+                GlobalUnlock(hg);
+                SetClipboardData(CF_TEXT, hg);
+                CloseClipboard();
+                GlobalFree(hg);
+                #endif
+                break;
+            }
+
+            case dynamic:
+            {
+                return;
+                break;
+            }
+
+            case qq:
+            {
+                return;
+                break;
+            }
+
+            case qzone:
+            {
+                return;
+                break;
+            }
+
+            case tieba:
+            {
+                return;
+                break;
+            }
+
+            case wechat:
+            {
+                return;
+                break;
+            }
+
+            case weibo:
+            {
+                return;
+                break;
+            }
+
         }
-        memcpy(GlobalLock(hg), len, size+1);
-        GlobalUnlock(hg);
-        SetClipboardData(CF_TEXT, hg);
-        CloseClipboard();
-        GlobalFree(hg);
-        #endif
     }
+        
+        
 
 }
