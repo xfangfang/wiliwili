1f2ac2d76bca6b17ea0c20bb940f5802e8f7592b
zeromake <a390720046@gmail.com>
2023-11-04 20:55:52 +0800

    fix: xmake use xrepo dev

--- xmake.lua
+++ xmake.lua
@@ -31,6 +31,8 @@ else
     set_languages("c++17")
 end
 
+add_repositories("zeromake https://github.com/zeromake/xrepo.git dev")
+
 package("borealis")
     set_sourcedir(path.join(os.projectdir(), "library/borealis"))
 
@@ -78,25 +80,6 @@ package("borealis")
         os.cp("library/include/*", package:installdir("include").."/")
         os.rm(package:installdir("include/borealis/extern"))
         os.cp("library/include/borealis/extern/libretro-common", package:installdir("include").."/")
-    end)
-package_end()
-
-package("mongoose")
-    set_urls("https://github.com/xfangfang/mongoose/archive/8a9b6556816cd5ec8b90f451af7bafd93b000b89.zip")
-
-    add_versions("latest", "01baf387a982e0c1ab32b12b41969d9f7ff05b5c834c1bc82a910572c119d115")
-    
-    on_install(function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("mongoose")
-                set_kind("$(kind)")
-                add_headerfiles("mongoose.h")
-                add_files("mongoose.c")
-        ]])
-        local configs = {}
-        configs["kind"] = package:config("shared") and "shared" or "static"
-        import("package.tools.xmake").install(package, configs)
     end)
 package_end()
 
@@ -162,7 +145,6 @@ package("mpv")
     end)
 package_end()
 
-add_repositories("zeromake https://github.com/zeromake/xrepo.git")
 if get_config("winrt") then
     add_requires("sdl2", {configs={shared=true,winrt=true}})
     add_requireconfs("**.sdl2", {configs={shared=true,winrt=true}})
@@ -249,11 +231,9 @@ target("wiliwili")
                 import("uwp")(target)
             else
                 for _, pkg in pairs(target:pkgs()) do
-                    if pkg:has_shared() then
-                        for _, f in ipairs(pkg:libraryfiles()) do
-                            if f:endswith(".dll") or f:endswith(".so") then
-                                os.cp(f, target:targetdir().."/")
-                            end
+                    for _, f in ipairs(pkg:libraryfiles()) do
+                        if f:endswith(".dll") or f:endswith(".so") then
+                            os.cp(f, target:targetdir().."/")
                         end
                     end
                 end
