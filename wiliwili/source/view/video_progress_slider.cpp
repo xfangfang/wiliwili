d83b2205a8670b4cec546dd2518aa17c9c4357b0
蜻蜓特派员 <dragonflylee@outlook.com>
2023-11-17 16:02:31 +0800

    Add tap support for progress slider (#242)

--- wiliwili/source/view/video_progress_slider.cpp
+++ wiliwili/source/view/video_progress_slider.cpp
@@ -82,6 +82,16 @@ VideoProgressSlider::VideoProgressSlider() {
         },
         PanAxis::HORIZONTAL));
 
+    this->addGestureRecognizer(new TapGestureRecognizer(
+        [this](brls::TapGestureStatus status, brls::Sound* soundToPlay) {
+            float paddingWidth = getWidth() - pointer->getWidth();
+            float delta =
+                status.position.x - pointer->getWidth() / 2 - pointer->getX();
+            setProgress(progress + delta / paddingWidth);
+            progressSetEvent.fire(this->progress);
+            Application::giveFocus(this->getParentActivity()->getContentView());
+        }));
+
     progress = 0;
 }
 
