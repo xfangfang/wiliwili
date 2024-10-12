0cb3ecb72dd491760dc007e8efe05751b44b4337
xfangfang <2553041586@qq.com>
2024-10-12 22:37:40 +0800

    macOS: update to mpv 0.39.0

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -5,7 +5,7 @@
 
 if (APPLE)
 
-    if (MAC_IntelChip)
+    if (MAC_IntelChip OR MAC_IntelChip_Old)
         set(MAC_OS_ARCH x86_64)
     elseif (MAC_AppleSilicon)
         set(MAC_OS_ARCH arm64)
@@ -13,16 +13,26 @@ if (APPLE)
         set(MAC_OS_ARCH universal)
     endif ()
 
+    set(MACOS_PREBUILD_LINK)
+    set(MACOS_WEBP_VERSION "7.1.8")
+    if (MAC_IntelChip_Old)
+        set(MACOS_PREBUILD_LINK "https://github.com/xfangfang/wiliwili/releases/download/v0.1.0/macos_dylib_ffmpeg61_mpv36_old.tar.gz")
+        list(APPEND APP_PLATFORM_LIB
+                ${CMAKE_BINARY_DIR}/deps/lib/libboost_filesystem-mt.dylib
+                ${CMAKE_BINARY_DIR}/deps/lib/libssl.3.dylib
+                ${CMAKE_BINARY_DIR}/deps/lib/libcrypto.3.dylib)
+    else ()
+        set(MACOS_PREBUILD_LINK "https://github.com/xfangfang/wiliwili/releases/download/v0.1.0/macos_dylib_ffmpeg71_mpv39_${MAC_OS_ARCH}.tar.gz")
+        set(MACOS_WEBP_VERSION "7.1.9")
+    endif ()
+
     if (NOT DISABLE_WEBP)
         list(APPEND APP_PLATFORM_OPTION -DUSE_WEBP)
-        list(APPEND APP_PLATFORM_LIB ${CMAKE_BINARY_DIR}/deps/lib/libwebp.7.1.9.dylib)
+        list(APPEND APP_PLATFORM_LIB ${CMAKE_BINARY_DIR}/deps/lib/libwebp.${MACOS_WEBP_VERSION}.dylib)
     endif ()
 
     list(APPEND APP_PLATFORM_LIB
-            ${CMAKE_BINARY_DIR}/deps/lib/libmpv.2.dylib
-            ${CMAKE_BINARY_DIR}/deps/lib/libboost_filesystem-mt.dylib
-            ${CMAKE_BINARY_DIR}/deps/lib/libssl.3.dylib
-            ${CMAKE_BINARY_DIR}/deps/lib/libcrypto.3.dylib)
+            ${CMAKE_BINARY_DIR}/deps/lib/libmpv.2.dylib)
     list(APPEND APP_PLATFORM_INCLUDE
             ${CMAKE_BINARY_DIR}/deps/include)
 
@@ -30,15 +40,13 @@ if (APPLE)
     include(FetchContent)
     if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
         cmake_policy(SET CMP0135 NEW)
-    endif()
-    FetchContent_Declare(macos_prebuild
-            URL "https://github.com/xfangfang/wiliwili/releases/download/v0.1.0/macos_dylib_ffmpeg7_mpv38_${MAC_OS_ARCH}.tar.gz"
-    )
+    endif ()
+    FetchContent_Declare(macos_prebuild URL ${MACOS_PREBUILD_LINK})
     if (NOT macos_prebuild_POPULATED)
         FetchContent_POPULATE(macos_prebuild)
         file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/deps)
         file(COPY ${macos_prebuild_SOURCE_DIR}/lib DESTINATION ${CMAKE_BINARY_DIR}/deps)
         file(COPY ${macos_prebuild_SOURCE_DIR}/include DESTINATION ${CMAKE_BINARY_DIR}/deps)
-    endif()
+    endif ()
 
 endif ()
--- scripts/macos_dylib_merge.sh
+++ scripts/macos_dylib_merge.sh
@@ -2,43 +2,25 @@
 set -e
 
 files=(libass.9.dylib
-       libavcodec.61.3.100.dylib
-       libavdevice.61.1.100.dylib
-       libavfilter.10.1.100.dylib
-       libavformat.61.1.100.dylib
-       libavutil.59.8.100.dylib
-       libboost_atomic-mt.dylib
-       libboost_filesystem-mt.dylib
-       libcrypto.3.dylib
+       libavcodec.61.19.100.dylib
+       libavfilter.10.4.100.dylib
+       libavformat.61.7.100.dylib
+       libavutil.59.39.100.dylib
        libdav1d.7.dylib
-       libfontconfig.1.dylib
        libfreetype.6.dylib
        libfribidi.0.dylib
-       libglib-2.0.0.dylib
-       libgmp.10.dylib
-       libgnutls.30.dylib
-       libgraphite2.3.2.1.dylib
        libharfbuzz.0.dylib
-       libhogweed.6.9.dylib
-       libidn2.0.dylib
-       libintl.8.dylib
-       libjpeg.8.3.2.dylib
-       liblcms2.2.dylib
+       libluajit-5.1.2.1.1727870382.dylib
+       libmbedcrypto.3.6.1.dylib
+       libmbedtls.3.6.1.dylib
+       libmbedx509.3.6.1.dylib
        libmpv.2.dylib
-       libnettle.8.9.dylib
-       libp11-kit.0.dylib
-       libpcre2-8.0.dylib
-       libplacebo.338.dylib
+       libplacebo.349.dylib
        libpng16.16.dylib
-       libshaderc_shared.1.dylib
        libsharpyuv.0.1.0.dylib
-       libssl.3.dylib
-       libswresample.5.1.100.dylib
-       libswscale.8.1.100.dylib
-       libtasn1.6.dylib
+       libswresample.5.3.100.dylib
+       libswscale.8.3.100.dylib
        libunibreak.6.dylib
-       libunistring.5.dylib
-       libvulkan.1.3.280.dylib
        libwebp.7.1.9.dylib)
 
 rm -rf ./universal
@@ -48,7 +30,7 @@ i=1
 for file in "${files[@]}"; do
   echo $i "$file"
   arm64_ret=$(otool -l ./arm64/"$file" | { grep minos || true;} | { grep 11.0 || true;})
-  x86_ret=$(otool -l ./x86_64/"$file" | { grep 10.11 || true;})
+  x86_ret=$(otool -l ./x86_64/"$file" | { grep 10.15 || true;})
 
   if [ -z "$arm64_ret" ]; then
       echo -e "\t\033[31m arm64 不满足 11.0 \033[0m"
@@ -62,9 +44,7 @@ for file in "${files[@]}"; do
   ((i++))
 done
 
-cp -r ./arm64/cmake ./universal/
-
-output_name="macos_dylib_ffmpeg7_mpv38"
+output_name="macos_dylib_ffmpeg71_mpv39"
 arch_list=("arm64" "x86_64" "universal")
 for i in "${arch_list[@]}";
 do
