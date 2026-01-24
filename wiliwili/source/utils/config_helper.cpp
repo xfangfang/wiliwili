b284a217fab604fee87e08e6ba135edb14d437c7
xfangfang <2553041586@qq.com>
2026-01-24 15:52:55 +0800

    fix: regenerate buvid3 when ending with "infoc"

--- wiliwili/source/utils/config_helper.cpp
+++ wiliwili/source/utils/config_helper.cpp
@@ -24,6 +24,8 @@
 #include "utils/thread_helper.hpp"
 #include "utils/image_helper.hpp"
 #include "utils/config_helper.hpp"
+
+#include "pystring.h"
 #include "utils/crash_helper.hpp"
 #include "utils/vibration_helper.hpp"
 #include "utils/ban_list.hpp"
@@ -334,7 +336,8 @@ void ProgramConfig::setCookie(const Cookie& data) {
 
 Cookie ProgramConfig::getCookie() {
     // 生成虚假的 buvid3，在获取直播分区时需要此字段
-    if (!this->cookie.count("buvid3")) {
+    // buvid3 不应以 infoc 结尾，否则会报错误码 352
+    if (!this->cookie.count("buvid3") || pystring::endswith(this->cookie["buvid3"], "infoc")) {
         this->cookie["buvid3"] = wiliwili::getRandomHex(32, false);
     }
     // 生成虚假的 DedeUserID，在未登录时使用
